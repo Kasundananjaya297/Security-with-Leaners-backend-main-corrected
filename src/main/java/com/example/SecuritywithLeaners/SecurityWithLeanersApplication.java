1e7ea4d6f14eca2953ecc24c949a7c00fe586a5d
Kasun Dhananjaya <125995451+Kasundananjaya297@users.noreply.github.com>
2024-06-01 16:53:45 +0530

    before deploying

--- src/main/java/com/example/SecuritywithLeaners/SecurityWithLeanersApplication.java
+++ src/main/java/com/example/SecuritywithLeaners/SecurityWithLeanersApplication.java
@@ -6,19 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 
 
 @SpringBootApplication()
-public class SecurityWithLeanersApplication  extends SpringBootServletInitializer {
+public class SecurityWithLeanersApplication  extends SpringBootServletInitializer  {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityWithLeanersApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SecurityWithLeanersApplication.class);
 	}
 
 
